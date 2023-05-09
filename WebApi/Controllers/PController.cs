4376a2a6cd0c140f551a37721da757505e2affed
AndreasJensen <andreasjensen2300@gmail.com>
2023-05-09 15:01:52 +0200

    Fejl handler

--- WebApi/Controllers/PController.cs
+++ WebApi/Controllers/PController.cs
@@ -12,7 +12,7 @@ namespace WebApi.Controllers
         public PController()
         {
             /// er tom så der ikke bliver smidt de samme ting ud flere, når man henter GET.
-            /// ellers skal den måske laves om til et static objekt 
+            /// ellers skal den måske laves om til et static objekt
         }
 
         [HttpGet("count")] ///Tæller hvor mange Products der er
@@ -24,12 +24,27 @@ namespace WebApi.Controllers
         [HttpGet]
         public IActionResult Get() /// Henter Product list
         {
+            if (_products.Count == 0)
+            {
+                return NoContent();
+            }
+
             return Ok(_products);
         }
 
         [HttpPost]
         public ActionResult<Products> Post(Products product) /// Gør så vi kan POST et nyt 
         {
+            if (string.IsNullOrEmpty(product.Name)) /// Sender en fejl tilbage hvis der ikke er noget navn
+            {
+                return BadRequest("Product name cannot be empty.");
+            }
+
+            if (product.Price <= 0) /// Sender en fejl tilbage hvis prisen ikke er højere end 0
+            {
+                return BadRequest("Product price must be greater than 0.");
+            }
+
             product.Id = _products.Count + 1;
             _products.Add(product);
 
